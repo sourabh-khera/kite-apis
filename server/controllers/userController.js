939bdba31cc38f8c7bc3aa39c931f4df7f66109b
Sourabh <sourabh.khera@tothenew.com>
2020-09-04 14:00:51 +0530

    logs added

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,13 +1,15 @@
 const userService = require("../services/userService");
-const logger = require("../configurations/logger");
 const isEmpty = require("lodash/isEmpty");
 const { HTTP_CODE, MESSAGES } = require("../constants");
 const { getAccessTokenSchema } = require("../joiSchema");
 const { sendAndLogError } = require("../utils");
+const logger = require("../configurations/logger");
 
 exports.getAccessToken = async (req, res) => {
   try {
+    logger.info("Starting execution of getAccessToken");
     const value = await getAccessTokenSchema.validateAsync(req.query);
+    logger.info(`request token = ${value.requestToken}`);
     const response = await userService.getAccessToken(value.requestToken);
     if (!isEmpty(response.data) && response.data.status === "success") {
       const {
